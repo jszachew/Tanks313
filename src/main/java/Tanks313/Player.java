327c5e23519897da0ccee7ea48428145c7e15aed
JSzachew <kubaszach@me.com>
2018-05-07 19:21:46 +0200

    Simple player, weapon and enemy class added - to extend!!

--- src/main/java/Tanks313/Player.java
+++ src/main/java/Tanks313/Player.java
@@ -1,4 +1,18 @@
 package Tanks313;
 
 public class Player {
+    private Weapon weapon;
+    private int damageBase;
+
+    public Player(int damageBase, Weapon weapon) {
+        this.damageBase = damageBase;
+        this.weapon = weapon;
+    }
+
+    public void attack(Enemy enemy) {
+        int damage = damageBase + weapon.rollDamage();
+        enemy.hit(damage);
+
+        System.out.printf("Player hit enemy for %s damage, enemy is at %s health\n", damage, enemy.getHealth());
+    }
 }
